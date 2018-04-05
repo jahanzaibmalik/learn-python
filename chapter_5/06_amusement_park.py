3d3e7b403ee89c11f5429a7173a0156f2d273878
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-05 23:19:18 +0500

    python practice

--- chapter_5/06_amusement_park.py
+++ chapter_5/06_amusement_park.py
@@ -2,20 +2,51 @@
 # age = 12
 #
 # if age < 4:
-#     print("Your admission cost is $0.")
+#     print("Your admission cost is $0")
 # elif age < 18:
-#     print("Your admission cost is $5.")
+#     print("Your admission cost is $5")
 # else:
-#     print("Your admission cost is $10.")
+#     print("Your admission cost is $10")
 
 
+# age = 12
+#
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 5
+# else:
+#     price = 10
+#
+# print("Your admission cost is $" + str(price) + ".")
+
+
+
+# Using Multiple elif Blocks
+# age = 12
+#
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 5
+# elif age < 65:
+#     price = 10
+# else:
+#     price = 5
+#
+# print("Your admission cost $" + str(price) + ".")
+
+
+# Omitting the else Block
 age = 12
 
 if age < 4:
     price = 0
 elif age < 18:
     price = 5
-else:
+elif age < 65:
     price = 10
+elif age >= 65:
+    price = 5
 
 print("Your admission cost is $" + str(price) + ".")
