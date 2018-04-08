8e51d4ae2d4121b96660450f12ac47c21844ac94
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-08 20:57:28 +0500

    python practice

---
+++ chapter_5/exercises/5-6_stages_of_life.py
@@ -0,0 +1,14 @@
+age = 18
+
+if age < 2:
+    print("You're an baby!")
+elif age < 4:
+    print("You're an toddler!")
+elif age < 13:
+    print("You're an kid!")
+elif age < 20:
+    print("You're an teenager!")
+elif age < 65:
+    print("You're an adult!")
+else:
+    print("You're an elder!")
