643713a33b074e2bb821b1cd029657790557a75d
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-08 21:09:35 +0500

    python practice

---
+++ chapter_5/exercises/5-7_favorite_fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['apples', 'bananas', 'oranges']
+
+if 'apples' in favorite_fruits:
+    print("You really like Apples")
+if 'lychees' in favorite_fruits:
+    print("You really like Lychees!")
+if 'bananas' in favorite_fruits:
+    print("You really like Bananas!")
+if 'grapes' in favorite_fruits:
+    print("You really like Grapes!")
+if 'oranges' in favorite_fruits:
+    print("You really like Oranges!")
