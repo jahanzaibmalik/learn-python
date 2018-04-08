b4ffe82263e63d5bbcefe020c7cbdac1e272f40d
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-08 19:21:46 +0500

    testing multiple conditions

--- chapter_5/07_toppings.py
+++ chapter_5/07_toppings.py
@@ -1,10 +1,22 @@
+# Testing Multiple Conditions
+# requested_toppings = ['mushrooms', 'extra cheese']
+#
+# if 'mushrooms' in requested_toppings:
+#     print("Adding mushrooms")
+# if 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni.")
+# if 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese.")
+#
+# print("\nFinished making pizza!")
+
 requested_toppings = ['mushrooms', 'extra cheese']
 
 if 'mushrooms' in requested_toppings:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
+     print("Adding mushrooms.")
+elif 'pepperoni' in requested_toppings:
+     print("Adding pepperoni.")
+elif 'extra cheese' in requested_toppings:
+     print("Adding extra cheese.")
 
-print("\nFinished making your pizza.")
+print("\nFinished making pizza!")
