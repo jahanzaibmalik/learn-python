1f6470c2280dfe371ef66009c35109911ccf4976
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-09 14:34:48 +0500

    python practice

---
+++ chapter_5/04_using_if_statements_with_lists/01_toppings.py
@@ -0,0 +1,7 @@
+# Checking for Special Items
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
