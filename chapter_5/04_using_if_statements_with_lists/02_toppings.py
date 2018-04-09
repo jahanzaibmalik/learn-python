81a82a6e96ecee62d6d28bff1accf42ca4b4bfd3
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-09 15:37:00 +0500

    Checking That a List Is Not Empty

---
+++ chapter_5/04_using_if_statements_with_lists/02_toppings.py
@@ -0,0 +1,10 @@
+# Checking That a List Is Not Empty
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
