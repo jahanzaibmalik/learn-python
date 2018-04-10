eed1a94c534e6279d05b7a2b8c0c289815efbd5f
= <=>
2018-04-10 19:30:52 +0500

    Python practice

---
+++ chapter_6/02_working_with_dictionaries/exercises/6-1_person.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'ahsan',
+    'last_name': 'ali',
+    'age': 20,
+    'city': 'karachi'
+}
+
+print(person['first_name'].title())
+print(person['last_name'].title())
+print(person['age'])
+print(person['city'].title())
