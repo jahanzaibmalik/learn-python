4fbe9d96c369ecd1c832772b5159542ac693b2d1
= <=>
2018-04-10 19:58:07 +0500

    Python practice

---
+++ chapter_6/02_working_with_dictionaries/exercises/6-2_favorite_numbers.py
@@ -0,0 +1,23 @@
+favorite_numbers = {
+    'ali': 5,
+    'shoaib': 4,
+    'shahrukh': 9,
+    'ahsan': 3,
+    'qasim': 52
+}
+
+num = favorite_numbers['ali']
+print("Ali's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['shoaib']
+print("Shoaib's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['shahrukh']
+print("Shahrukh's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['ahsan']
+print("Ahsan's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['qasim']
+print("Qasim's favorite number is " + str(num) + ".")
+
