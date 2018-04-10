78ca59dff00bab7dabdeee02e340c4462a1c2c19
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-10 17:38:56 +0500

    Accessing Values in a Dictionary

---
+++ chapter_6/02_working_with_dictionaries/01_alien.py
@@ -0,0 +1,16 @@
+# Accessing Values in a Dictionary
+# alien_0 = {'color': 'green'}
+# print(alien_0['color'])
+
+
+# alien_0 = {'color': 'green', 'points': 5}
+#
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+
+alien_0 = {'color': 'green', 'points': 5}
+
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points")
+
