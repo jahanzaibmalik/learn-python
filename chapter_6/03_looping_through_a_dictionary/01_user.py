02d12405fe8e3dc930122c11a588be397a475b69
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-11 16:46:05 +0500

    # Looping Through All Key-Value Pairs

---
+++ chapter_6/03_looping_through_a_dictionary/01_user.py
@@ -0,0 +1,10 @@
+# Looping Through All Key-Value Pairs
+user_0 = {
+    'username': 'shahrukh',
+    'first': 'jahanzaib',
+    'last': 'malik'
+}
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
