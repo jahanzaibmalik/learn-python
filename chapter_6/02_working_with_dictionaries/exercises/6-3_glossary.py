903762c96ca06399aef93c6cc87b0b714001318c
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-11 15:59:59 +0500

    Python practice

---
+++ chapter_6/02_working_with_dictionaries/exercises/6-3_glossary.py
@@ -0,0 +1,22 @@
+glossary = {
+    'variable': 'Variables are used to store information.',
+    'list': 'A list is a number of items in an ordered or unordered structure.',
+    'if-statements': 'An if statement is a conditional statement.',
+    'loops': 'A loop is a sequence of instruction.',
+    'dictionaries': 'A collection of key-value pairs.'
+}
+
+word = 'variable'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'list'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'if-statements'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'loops'
+print("\n" + word.title() + ": " + glossary[word])
+
+word = 'dictionaries'
+print("\n" + word.title() + ": " + glossary[word])
