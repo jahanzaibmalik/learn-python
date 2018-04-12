7ab3fedd32943c7c34fca255109ee903ba5d97c8
jahanzaibmalik <jahanzaibmalik6@gmail.com>
2018-04-12 22:59:18 +0500

    Looping Through a Dictionary’s Keys in Order

---
+++ chapter_6/03_looping_through_a_dictionary/03_looping_through_a_dictionary's_keys_in_order/01_favorite_languages.py
@@ -0,0 +1,10 @@
+# Looping Through a Dictionary’s Keys in Order
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking poll.")
