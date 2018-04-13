1dcaa78a634a028f3f0787a84b6e8a75c20dc05f
= <=>
2018-04-13 20:10:53 +0500

    Looping Through All Values in a Dictionary

---
+++ chapter_6/03_looping_through_a_dictionary/04_looping_through_all_values_in_a_dictionary/02_favorite_languages.py
@@ -0,0 +1,11 @@
+# Looping Through All Values in a Dictionary
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
