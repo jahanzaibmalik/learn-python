6a018ca31fd2751e491959fb2bcc404d62231067
= <=>
2018-04-15 02:19:58 +0500

    A list of Dictionaries

--- chapter_6/04_nesting/01_a_list_of_dictionaries/02_aliens.py
+++ chapter_6/04_nesting/01_a_list_of_dictionaries/02_aliens.py
@@ -1,41 +1,13 @@
-# A List of Dictionaries
-# aliens = []
-# for value in range(30):
-#     new_alien = {'speed': 'slow', 'color': 'green', 'points': 5}
-#     aliens.append(new_alien)
-#
-# for alien in aliens[:5]:
-#     print(alien)
-#
-# print("...")
-# print("\nTotal number of aliens: " + str(len(aliens)))
-
-
+# A list of Dictionaries
+# Make an empty list for storing aliens.
 aliens = []
+
 for alien_number in range(30):
-    new_alien = {'speed': 'slow', 'color': 'green', 'points': 5}
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
-
-for alien in aliens[0:3]:
-    if alien['color'] == 'green':
-        alien['color'] = 'yellow'
-        alien['speed'] = 'medium'
-        alien['points'] = 10
-
-    # elif alien['color'] == 'yellow':
-    #     alien['color'] = 'red'
-    #     alien['speed'] = 'fast'
-    #     alien['points'] = 15
-
-for alien in aliens[0:3]:
-    if alien['color'] == 'yellow':
-        alien['color'] = 'red'
-        alien['speed'] = 'fast'
-        alien['points'] = 15
 
 for alien in aliens[:5]:
     print(alien)
-
 print("...")
-print("\nTotal number of aliens: " + str(len(aliens)))
 
+print("Total number of aliens: " + str(len(aliens)))
