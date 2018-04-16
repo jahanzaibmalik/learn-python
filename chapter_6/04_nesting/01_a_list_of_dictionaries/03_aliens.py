aa276e54dfad55b1def6163edd8c1edb171ffcc6
= <=>
2018-04-16 17:44:51 +0500

    A List of Dicitionaries

---
+++ chapter_6/04_nesting/01_a_list_of_dictionaries/03_aliens.py
@@ -0,0 +1,19 @@
+# A List of Dictionaries
+aliens = []
+
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+
+for alien in aliens[:5]:
+    print(alien)
+
+print("...")
+
+print("\nTotal number of aliens: " + str(len(aliens)))
