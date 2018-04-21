53326522547ff73486c4553ef4999f7267205b2b
= <=>
2018-04-21 19:22:13 +0500

    Using int() to Accept Numerical Input

---
+++ chapter_7/02_using_int()_to_accept_numerical_input/01_rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("How old are you, in inches? ")
+height = int(height)
+
+if height >= 36:
+    print("\nYou are old enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
