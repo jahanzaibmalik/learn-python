7754e4e1ca4bc71c231a0963f8c0872331c043fd
= <=>
2018-04-21 19:25:45 +0500

    How the input() Function Works

---
+++ chapter_7/01_how_the_input()_function_works/01_parrot.py
@@ -0,0 +1,8 @@
+# message  = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+
+print("Your name is " + full_name.title())
